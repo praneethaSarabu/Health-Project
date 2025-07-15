dad1abd3496adcdacaac8999db75fbd59e1b56b1
praneethaSarabu <praneethasvn@gmail.com>
2025-07-15 23:19:04 +0530

    Initial commint - Health Project

--- src/main/java/com/health/dao/DBUtil.java
+++ src/main/java/com/health/dao/DBUtil.java
@@ -4,9 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBUtil {
-    private static final String URL = "jdbc:oracle:thin:@localhost:1522:XE"; // Oracle default port
-    private static final String USER = "system"; // use your Oracle username
-    private static final String PASS = "newpassword123"; // replace with your real Oracle password
+    private static final String URL = "jdbc:oracle:thin:@localhost:1522:XE";
+    private static final String USER = "system"; 
+    private static final String PASS = "newpassword123"; 
 
     public static Connection getConnection() throws Exception {
         Class.forName("oracle.jdbc.driver.OracleDriver");
